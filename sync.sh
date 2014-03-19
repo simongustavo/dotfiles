332ef7efc7deac7cc2e81038ed03bddc549a683d
Gustavo Simon <dev@gustavosimon.com>
2014-03-18 21:04:51 -0400

    .inputrc sourced

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="aliases bash_profile bash_prompt bashrc exports functions vimrc vim"    # list of files/folders to symlink in homedir
+files="inputrc aliases bash_profile bash_prompt bashrc exports functions vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
